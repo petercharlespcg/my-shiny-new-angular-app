210ed276b31589b23c55cf4bf6069d935df7e089
Peterg <petercharles.ca@gmail.com>
2020-08-24 10:58:46 -0400

    CanDeactivate Guard in Angular

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,26 +1,35 @@
+import { ConfirmationGuard } from './../guards/confirmation/confirmation.guard';
+import { MessageListComponent } from './../components/message-list/message-list.component';
+import { AuthGuard } from './../guards/auth/auth.guard';
 import { UserDetailsComponent } from './../components/user-details/user-details.component';
 import { UsersComponent } from './../components/users/users.component';
 import { AboutComponent } from './../components/about/about.component';
 import { BlogComponent } from './../components/blog/blog.component';
 import { HomeComponent } from './../components/home/home.component';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, CanDeactivate } from '@angular/router';
 import { NgModule, Component } from '@angular/core';
 import { PlaceholderComponent } from '../components/placeholder/placeholder.component';
 // import { CommonModule } from '@angular/common';
 
 const appRoutes: Routes = [
   { path: 'home', component: HomeComponent },
-  { path: 'blog', component: BlogComponent },
+  { path: 'blog', component: BlogComponent, canActivate: [ AuthGuard ] },
   { path: 'about', component: AboutComponent },
-  { path: 'users', component: UsersComponent, pathMatch: 'prefix', children: [
+  {
+    path: 'users',
+    component: UsersComponent,
+    pathMatch: 'prefix',
+    canActivateChild: [ AuthGuard ],
+    children: [
       { path: ':userId', component: UserDetailsComponent },
-      { path: '', component: PlaceholderComponent }
+      // { path: '', component: PlaceholderComponent }
   ] },
+  { path: 'messages', component: MessageListComponent, canDeactivate: [ ConfirmationGuard ]},
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ]
 
 @NgModule({
-  // declarations: [],
+  providers: [ AuthGuard ],
   imports: [ RouterModule.forRoot(appRoutes) ],
   exports: [ RouterModule ]
 })
--- src/app/components/message-list/message-list.component.ts
+++ src/app/components/message-list/message-list.component.ts
@@ -1,12 +1,13 @@
 import { MessageComponent } from './../message/message.component';
 import { Component, OnInit, ViewChild, AfterViewInit, ViewChildren, QueryList, ContentChild, AfterContentInit, ContentChildren } from '@angular/core';
+import { CanComponentDeactivate } from "../../guards/confirmation/confirmation.guard";
 
 @Component({
   selector: 'app-message-list',
   templateUrl: './message-list.component.html',
   styleUrls: ['./message-list.component.scss']
 })
-export class MessageListComponent implements OnInit, AfterViewInit, AfterContentInit {
+export class MessageListComponent implements OnInit, AfterViewInit, AfterContentInit, CanComponentDeactivate {
 
   @ViewChild(MessageComponent) firstMessageComponent: MessageComponent;
   @ViewChildren(MessageComponent) allMessageComponents: QueryList<MessageComponent>;
@@ -24,6 +25,9 @@ export class MessageListComponent implements OnInit, AfterViewInit, AfterContent
 
   constructor() {
     // this.firstMessageComponent.message = 'Changed the message from message list constructor';
+  }
+  confirm(): boolean {
+    return confirm('Are you sure you want to navigate away?');
   }
   ngAfterContentInit(): void {
     // this.firstProjectedMessageComponent.message = 'This was changed from message list component\'s ngAfterContentInit method';
